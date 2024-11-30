5eb22ba8d8833494eafe72556e14ef434b0f87fb
Arthur <arthurg.gmarques@gmail.com>
2024-11-30 15:24:48 -0300

    Algumas mudanças no config do tailwind.

--- repositorio/tailwind.config.js
+++ repositorio/tailwind.config.js
@@ -1,7 +1,5 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
- 
 const svgToDataUri = require("mini-svg-data-uri");
- 
 const colors = require("tailwindcss/colors");
 const {
   default: flattenColorPalette,
@@ -75,10 +73,21 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        spotlight: "spotlight 2s ease .75s 1 forwards",
       },
     },
   },
@@ -108,13 +117,12 @@ module.exports = {
   ],
 };
 
- 
 function addVariablesForColors({ addBase, theme }) {
   let allColors = flattenColorPalette(theme("colors"));
   let newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
   );
- 
+
   addBase({
     ":root": newVars,
   });
